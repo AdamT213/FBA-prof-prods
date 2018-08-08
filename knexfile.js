93415e397bdb32e50143331857fab6d12e35e267
Adam <adt6261@gmail.com>
2018-08-08 16:39:58 -0400

    hardcoded production db url

--- knexfile.js
+++ knexfile.js
@@ -31,7 +31,7 @@ module.exports = {
 
   production: { 
     client: 'postgresql', 
-    connection: process.env.PRODUCTION_DB,
+    connection: 'postgres://yzseonoxkkrykt:fa3f28caec77f6347446f3a0324280ad6bba6c191cfa884b27cb526365dd911f@ec2-107-21-98-165.compute-1.amazonaws.com:5432/dba646b89tvmfb',
     pool: {
       min: 2,
       max: 10
