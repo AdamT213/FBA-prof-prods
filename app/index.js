9c344453947ccf7e86d74bde9c955c11ae7d3fcb
Adam <adt6261@gmail.com>
2018-08-08 18:30:04 -0400

    testing console.log on each line to see where problem is ocurring

--- app/index.js
+++ app/index.js
@@ -88,21 +88,22 @@ router.post('/distributor/:id/upload', (req,res) => {
   csv()
   .fromString(req.body.toString('utf8'))
   .on('json', (item) => { 
-    item.distributor_id = distributor.id 
-    Product
-      .forge(item.body)
-      .save()
-      .then((product) => {
-        res.json({id: product.id});
-      })
-      .catch((error) => {
-        console.error(error);
-        return res.sendStatus(500);
-       })
-    })
-    .on('done', () => {
-      console.log('done parsing');
-    });
+    console.log(item)
+    // item.distributor_id = distributor.id 
+    // Product
+    //   .forge(item.body)
+    //   .save()
+    //   .then((product) => {
+    //     res.json({id: product.id});
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //     return res.sendStatus(500);
+    //    })
+    // })
+    // .on('done', () => {
+    //   console.log('done parsing');
+    // });
 }); 
 
 // Exports for Server Hoisting.
