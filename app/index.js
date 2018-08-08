79f84ac2967826218bdbb6353d5ac5ee4b51082d
Adam <adt6261@gmail.com>
2018-08-08 18:12:03 -0400

    examining req object to see where program is breaking

--- app/index.js
+++ app/index.js
@@ -85,20 +85,21 @@ router.post('/distributors', (req, res) => {
 }); 
 
 router.post('/distributor/:id/upload', (req,res) => { 
-  csv()
-  .fromString(req.files.data.toString('utf8'))
-  .on('json', (item) => { 
-    item.distributor_id = distributor.id 
-    Product
-      .forge(item.body)
-      .save()
-      .then((product) => {
-        res.json({id: product.id});
-      })
-      .catch((error) => {
-        console.error(error);
-        return res.sendStatus(500);
-       })
+  console.log(req)
+  // csv()
+  // .fromString(req.files.data.toString('utf8'))
+  // .on('json', (item) => { 
+  //   item.distributor_id = distributor.id 
+  //   Product
+  //     .forge(item.body)
+  //     .save()
+  //     .then((product) => {
+  //       res.json({id: product.id});
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //       return res.sendStatus(500);
+  //      })
     })
     .on('done', () => {
       console.log('done parsing');
