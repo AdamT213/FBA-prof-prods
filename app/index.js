cff0f98da1d8b8ec99a45d911e6e0a2271e9623f
Adam <adt6261@gmail.com>
2018-08-09 18:16:58 -0400

    trying to acces string inside object

--- app/index.js
+++ app/index.js
@@ -91,7 +91,7 @@ router.post('/distributor/:id/upload', upload.single(), function (req, res, next
   console.log(req.body) 
   next()
 }, function (req, res, next) { 
-    console.log(req.body.data)
+    console.log(req.body[0])
     // csv({output:"line"})
     // .fromString(JSON.stringify(req.body))
     // .subscribe((csvLine)=>{
