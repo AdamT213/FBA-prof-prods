08dd37d2666bb6105da79c916d4091d7edd1f5a6
Adam <adt6261@gmail.com>
2018-08-09 14:28:20 -0400

    experimenting with trying to handle rejection

--- app/index.js
+++ app/index.js
@@ -89,12 +89,12 @@ router.post('/distributors', (req, res) => {
 router.post('/distributor/:id/upload', (req,res) => { 
   return new Promise((resolve, reject) => {
     upload(req,res,function(err){
-      if(err !== null) 
-        reject(err);
-        return res.sendStatus(500);
       console.log(res)
       resolve(res);
-    });
+    })
+    .catch((err) => { 
+      reject(err);
+    })
   })
   // .then((req, res) => {
   //   console.log(req)
