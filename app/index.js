5eb23b79f91d998c9f46c626e15d89ddb0473f04
Adam <adt6261@gmail.com>
2018-08-09 17:01:09 -0400

    still trying to see each line individually

--- app/index.js
+++ app/index.js
@@ -94,9 +94,8 @@ router.post('/distributor/:id/upload', upload.single(), function (req, res, next
     // console.log(req.body.toString('utf8'));
     csv()
     .fromString(req.body.toString('utf8'))
-    .on('json', (item) => { 
-      console.log(item)
-      break
+    .then((jsonObj)=>{
+      console.log(jsonObj);
       // item.distributor_id = req.params.id 
       // Product
       // .forge(item.body)
