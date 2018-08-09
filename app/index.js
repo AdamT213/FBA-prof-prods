a0db8bccd1d58975e36a1ff4983a5139163b4423
Adam <adt6261@gmail.com>
2018-08-08 22:31:38 -0400

    added full upload functionality inside promise

--- app/index.js
+++ app/index.js
@@ -87,14 +87,16 @@ router.post('/distributors', (req, res) => {
 }); 
 
 router.post('/distributor/:id/upload', (req,res) => { 
-  return new Promise((upload) => {
-    console.log("We have received your file") 
+  return new Promise(() => {
+    upload(req, res, function (err) {
+      if (err) {
+        console.error("An error occurred when uploading. Please try again. Note that you may only upload one file at a time, and we only support .csv files.")
+        return
+      }
+      console.log("We have received your file")
+    }) 
   })
-  .catch((error) => {
-    console.error(error);
-    return res.sendStatus(500);
-  })
-  .then((res) => {
+  .then(() => {
     csv()
     .fromString(req.body.toString('utf8'))
     .on('json', (item) => { 
