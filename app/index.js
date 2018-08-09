321c855921e222b543e746b498b1ed9340c70f4a
Adam <adt6261@gmail.com>
2018-08-08 23:45:32 -0400

    ostensibly fixed error with multer

--- app/index.js
+++ app/index.js
@@ -87,7 +87,7 @@ router.post('/distributors', (req, res) => {
 }); 
 
 router.post('/distributor/:id/upload', (req,res) => { 
-  return upload()((req, res, err) => {
+  return upload(req, res, function(err) {
     if (err) {
       console.error("An error occurred when uploading. Please try again. Note that you may only upload one file at a time, and we only support .csv files.")
       return
