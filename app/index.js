d07bdb5ca2c478e42ee0a9bea0c482ae53304552
Adam <adt6261@gmail.com>
2018-08-09 16:13:08 -0400

    trying to get the value of each line as an item

--- app/index.js
+++ app/index.js
@@ -88,10 +88,10 @@ router.post('/distributors', (req, res) => {
 
 router.post('/distributor/:id/upload', upload.single(), function (req, res, next) {
   req.setTimeout(600000);
-  console.log(req.body); 
+  // console.log(req.body); 
   next()
 }, function (req, res, next) {
-    console.log(req.body)
+    // console.log(req.body)
     return csv()
     .fromString(req.body.toString('utf8'))
     .on('json', (item) => { 
