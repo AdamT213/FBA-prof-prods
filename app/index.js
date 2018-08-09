6ab26b46bfb5eca2cc81acd95e5056b271d9a522
Adam <adt6261@gmail.com>
2018-08-09 17:18:20 -0400

    seeing what req.body returns inside function

--- app/index.js
+++ app/index.js
@@ -91,7 +91,7 @@ router.post('/distributor/:id/upload', upload.single(), function (req, res, next
   // console.log(req.body); 
   next()
 }, function (req, res, next) {
-    console.log(req.body.toString('utf8'));
+    console.log(req.body);
     // csv({output:"line"})
     // .fromString(req.body.toString('utf8'))
     // .subscribe((csvLine)=>{
