2b208e5147d5d9aa4d2fcf3d22cf250601e5f40e
Adam <adt6261@gmail.com>
2018-08-14 13:01:19 -0400

    using superagent to send request and moment.js to set timestamp. Works, but getting signature error

--- app/calculateSignature.js
+++ app/calculateSignature.js
@@ -1,12 +1,14 @@
 const CryptoJS = require("crypto-js");
+const agent = require('superagent') 
+const moment = require('moment')
+
+//moment.js UTC format seems to be the only thing MWS accepts as valid ISO 8601
+var timestamp = moment().utc().format("YYYY-MM-DDTHH:mm:ss") + "Z"
+
 
 exports.generateSignature = () => { 
 
-  var date = new Date() 
-  
-  var timestamp = encodeURIComponent(date.toISOString())
-
-  var Message = "POST" + "\n" + "https://mws.amazonservices.com/" + "\n"+ "/Products/2011-10-01" + "\n" + "AWSAccessKeyId=AKIAJO5TPTZ5YGGPNGQA&Action=GetMatchingProductForId&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=" + timestamp + "&Version=2011-10-01"; 
+  var Message = "POST" + "\n" + "https://mws.amazonservices.com/" + "\n" + "/Products/2011-10-01" + "\n" + "AWSAccessKeyId=" + encodeURIComponent('AKIAJO5TPTZ5YGGPNGQA') + "&Action=" + encodeURIComponent('GetMatchingProductForId') + "&SignatureMethod=" + encodeURIComponent('HmacSHA256') + "&SignatureVersion=" + encodeURIComponent('2') + "&Timestamp=" + encodeURIComponent(timestamp) + "&Version=" + encodeURIComponent("2011-10-01"); 
 
   var secret = "IrgC8kn+R2WirgIbM8N+hLHUjAS/6CLWvf1dzLcd";
 
@@ -14,7 +16,7 @@ exports.generateSignature = () => {
   
   var hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
   
-  return hashInBase64 
+  return encodeURIComponent(hashInBase64)
 
 } 
 
@@ -22,9 +24,32 @@ console.log(exports.generateSignature())
 
 // UPC=043171884536 
 //sellerId=A1N0R958ET8VVH 
+
+exports.sendRequest = () => {
  
-//set up fetch request to url constructed from query params, with generated signature
+  return agent
+    .post('https://mws.amazonservices.com/Products/2011-10-01')
+    .query({
+      AWSAccessKeyId: 'AKIAJO5TPTZ5YGGPNGQA',
+      Action: 'GetMatchingProductForId',
+      SellerId: 'A1N0R958ET8VVH',
+      SignatureVersion: '2',
+      Timestamp: timestamp,
+      Version: '2011-10-01',
+      Signature: exports.generateSignature(),
+      SignatureMethod: 'HmacSHA256',
+      MarketplaceId: 'ATVPDKIKX0DER',
+      IdType: 'UPC',
+      'IdList.Id.1': '043171884536'
+    })
+    .then(res => {
+      console.log('here is the response');
+      console.log(res)
+    })
+    .catch(error => {
+      console.log('here is the error');
+      console.log(error);
+    })
+} 
 
-//set up ngrok so i don't have to keep pushing heroku commits 
-
-//test fetch requests 
+console.log(exports.sendRequest())
