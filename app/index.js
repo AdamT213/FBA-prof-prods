9ae7633bb9751b5125238dbd7c8b025d7677e00b
Adam <adt6261@gmail.com>
2018-08-20 17:51:05 -0400

    trying with await syntax

--- app/index.js
+++ app/index.js
@@ -119,9 +119,9 @@ router.post('/distributor/:id/upload', upload.single('file'), function (err,req,
         }).then((product) => {
           // console.log(product) 
           var productInfo = getPriceandASIN.getPriceandASIN(product.UPC) 
-          return {productInfo, product}
+          return {product, await(productInfo)}
         }).then((data) => {  
-          console.log(await(data.productInfo))
+          console.log(data.productInfo)
           console.log(data.product)
           // console.log(product) 
           //will return null if no product matching UPC is found
