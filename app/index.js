060f72085a7dc56ab022c3a70b7df7f960ef73d2
Adam <adt6261@gmail.com>
2018-08-21 15:53:04 -0400

    adding back next step. Cannot tell why this is breaking

--- app/index.js
+++ app/index.js
@@ -148,13 +148,13 @@ router.post('/distributor/:id/upload', upload.single('file'), function (err,req,
           // console.log(resp)
           if (resp && resp.feeEstimateInfo) { 
             console.log(resp) 
-          //   // let product = resp.product 
-          //   // let amazonFees = resp.feeEstimateInfo 
-          //   // product.amazonFees = amazonFees 
+            let product = resp.product 
+            let amazonFees = resp.feeEstimateInfo 
+            product.amazonFees = amazonFees 
             
-          //   //calculate selling price - buying price - fees to see if product is profitable 
-          //   var profitability = (product.retailSellingPrice - product.Price - product.amazonFees > 0) 
-          //   // console.log(product)
+            //calculate selling price - buying price - fees to see if product is profitable 
+            var profitability = (product.retailSellingPrice - product.Price - product.amazonFees > 0) 
+            console.log(profitability)
              
           //   //save product to db if it is profitable
           //   // if (profitability == true) { 
