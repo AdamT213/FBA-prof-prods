63c1c177161e9b828cd9b67b55e461eed4a9db18
Adam <adt6261@gmail.com>
2018-08-21 15:31:54 -0400

    scaling back

--- app/index.js
+++ app/index.js
@@ -146,27 +146,27 @@ router.post('/distributor/:id/upload', upload.single('file'), function (err,req,
           return product
         }).then(resp => {   
           if (resp.feeEstimateInfo) { 
-            console.log(resp)
-            let product = resp.product 
-            let amazonFees = resp.feeEstimateInfo 
-            product.amazonFees = amazonFees 
+            console.log(resp.product)
+            // let product = resp.product 
+            // let amazonFees = resp.feeEstimateInfo 
+            // product.amazonFees = amazonFees 
             
             //calculate selling price - buying price - fees to see if product is profitable 
             var profitability = (product.retailSellingPrice - product.Price - product.amazonFees > 0) 
             // console.log(product)
              
             //save product to db if it is profitable
-            if (profitability == true) { 
-              product.isProfitable = true 
-              product.profitMargin = profitability/retailSellingPrice 
-              Product
-              .forge(product)
-              .save()
-              .then((prod) => {
-                console.log({id: prod.id}) 
-                res.json({id: prod.id});
-              }) 
-            }      
+            // if (profitability == true) { 
+            //   product.isProfitable = true 
+            //   product.profitMargin = profitability/retailSellingPrice 
+            //   Product
+            //   .forge(product)
+            //   .save()
+            //   .then((prod) => {
+            //     console.log({id: prod.id}) 
+            //     res.json({id: prod.id});
+            //   }) 
+            // }      
           } 
         }); 
       });
