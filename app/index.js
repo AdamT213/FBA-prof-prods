7acb479472a8d5ffc0739be23df4fb600b88003e
Adam <adt6261@gmail.com>
2018-08-22 15:18:15 -0400

    trying end of res at all different levels to see what works

--- app/index.js
+++ app/index.js
@@ -179,9 +179,9 @@ router.post('/distributor/:id/upload', upload.single('file'), function (err,req,
               .then((prod) => {
                 console.log({id: prod.id})  
               }) 
-            }  
-            res.end()    
+            }      
           }
+          res.end()
         })
       }); 
     })
