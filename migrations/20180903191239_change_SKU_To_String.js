ff9771b1cb9638eee2b7d75c3004fa41182cc244
Adam <adt6261@gmail.com>
2018-09-03 19:17:26 -0400

    changed sku to string to be more flexible. This should resolve the issue with saving products from this file

---
+++ migrations/20180903191239_change_SKU_To_String.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.alterTable('products', (tbl) => {
+      tbl.string('SKU').alter();
+    }),
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTable('products')
+};
